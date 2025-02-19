bc678ccf0165e1a2cacd79238bf45cf34aaa4ae2
YevYar <kpyto2012@gmail.com>
2025-02-20 00:52:00 +0200

    Smooth NPC rotation when moving

--- Source/ShootThemUp/Private/AI/STUAICharacter.cpp
+++ Source/ShootThemUp/Private/AI/STUAICharacter.cpp
@@ -3,10 +3,19 @@
 
 #include "AI/STUAICharacter.h"
 
+#include "GameFramework/CharacterMovementComponent.h"
+
 #include "AI/STUAIController.h"
 
 ASTUAICharacter::ASTUAICharacter(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
 {
     AutoPossessAI     = EAutoPossessAI::PlacedInWorldOrSpawned;
     AIControllerClass = ASTUAIController::StaticClass();
+
+    bUseControllerRotationYaw = false;
+    if (GetCharacterMovement())
+    {
+        GetCharacterMovement()->bUseControllerDesiredRotation = true;
+        GetCharacterMovement()->RotationRate                  = FRotator{0.0f, 200.0f, 0.0f};
+    }
 }
